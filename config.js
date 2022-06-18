9d8ed9947cf46410fc5ed4e4126f8ac779591965
ogenkidesuka <106530585+ogenkidesuka@users.noreply.github.com>
2022-06-18 21:07:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -37,14 +37,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','447441437150']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
-global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
+global.owner = ['6283894435611','6285878070363']
+global.premium = ['','']
+global.botname = 'Vams'
+global.ownername = 'Tn_vams'
+global.group1 = ''
+global.group2 = ''
+global.packname = 'Tn-vams'
+global.ytchannel = ''
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
